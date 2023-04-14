c5962d4018ba527e91aead6571c74ef27b68b137
王东川 <wangdc9@asiainfo.com>
2023-04-14 15:24:14 +0800

    feat: 0002 MyReturnType

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,5 @@
-type MyReturnType<T> = any
+type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer X ? X : any
+// 推断出函数T的返回类型
+// 收获:
+// 函数的返回值根据return 来生成联合类型
+// 函数的返回值也可以infer来声明
