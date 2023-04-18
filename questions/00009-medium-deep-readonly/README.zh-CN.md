41f29ea4bd4dfcb5eac099644d1ca7bf6a87e611
王东川 <wangdc9@asiainfo.com>
2023-04-18 18:17:57 +0800

    feat: 更新备注

--- questions/00009-medium-deep-readonly/README.zh-CN.md
+++ questions/00009-medium-deep-readonly/README.zh-CN.md
@@ -9,20 +9,20 @@
 例如
 
 ```ts
-type X = { 
-  x: { 
+type X = {
+  x: {
     a: 1
     b: 'hi'
   }
   y: 'hey'
 }
 
-type Expected = { 
-  readonly x: { 
+type Expected = {
+  readonly x: {
     readonly a: 1
     readonly b: 'hi'
   }
-  readonly y: 'hey' 
+  readonly y: 'hey'
 }
 
 type Todo = DeepReadonly<X> // should be same as `Expected`
--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,34 @@
 type DeepReadonly<T> = any
+
+// 首先js的实现方式
+let targetObjddd = {
+  a: '1',
+  b: '2',
+  c: {
+    b: '1',
+    d: '2',
+    f: {
+      g: '8',
+    },
+  },
+}
+
+function handleReadonly(obj) {
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    Object.keys(obj).forEach((item) => {
+      let key = item
+      if (Object.prototype.toString.call(obj) === '[object Object]') {
+        handleReadonly(item)
+      }
+      else {
+        Object.defineProperties(obj, {
+          [key]: {
+            writable: false,
+          },
+        })
+      }
+    })
+  }
+}
+handleReadonly(targetObjddd)
+targetObjddd.a = '放'
