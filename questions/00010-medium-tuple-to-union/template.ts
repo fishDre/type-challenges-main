65e7d7a608d8f60989de38822c7d030034508f4a
王东川 <wangdc9@asiainfo.com>
2023-04-19 18:26:51 +0800

    feat: 00010元组转联合类型

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,8 @@
-type TupleToUnion<T> = any
+// type TupleToUnion<T extends any[]> = keyof {
+//   [P in T[number]]: P
+// }
+type TupleToUnion<T extends unknown[]> = T[number]
+type A4 = [1, 2, 3, true]
+
+type A3 = { [P in keyof A4]: A4[P] }
+// 考察对数组索引签名的理解
