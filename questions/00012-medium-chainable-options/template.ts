bfc34e1a5facc041148f95de3d806be74e0fe5ed
王东川 <wangdc9@asiainfo.com>
2023-04-20 18:15:03 +0800

    feat: 00012Chainable链式调用

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,18 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T extends object = {}> = {
+  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<T & Record<K, V>>
+  get(): T
 }
+
+// <>里面仅仅约束不要里面写判断
+// never可以用来去除对象里面的键
+
+/// / All primitive types
+// type Primitive = string
+// | boolean | number
+// | bigint | symbol
+// | null | undefined
+
+// All non-primitive types
+// type NonPrimitive = object
+
+// 所有的非原始类型就是object
